66de494118fa60552efc18bffa15b5a36a4d49f5
tranchis <78248-tranchis@users.noreply.gitlab.aicrowd.com>
2023-12-15 16:02:42 -0600

    Fix state id in MATCH clause

--- pgeon/policy_graph.py
+++ pgeon/policy_graph.py
@@ -241,7 +241,7 @@ class PolicyGraph(nx.MultiDiGraph):
         for edge in self.edges:
             n_from, n_to, action = edge
             # TODO The identifier of an edge may need to be unique. Check and rework the action part of this if needed.
-            graph_string += f"\nMATCH (s{node_info[n_from]['id']}:State) WHERE {node_info[n_from]['id']}.uid = \"{node_info[n_from]['id']}\" MATCH ({node_info[n_to]['id']}:State) WHERE {node_info[n_to]['id']}.uid = \"{node_info[n_to]['id']}\" CREATE (s{node_info[n_from]['id']})-[:action " + "{" + f"aid: \"{action_info[action]['id']}\", probability:{self[n_from][n_to][action]['probability']}, frequency:{self[n_from][n_to][action]['frequency']}" + "}" + f"]->(s{node_info[n_to]['id']});"
+            graph_string += f"\nMATCH (s{node_info[n_from]['id']}:State) WHERE s{node_info[n_from]['id']}.uid = \"s{node_info[n_from]['id']}\" MATCH (s{node_info[n_to]['id']}:State) WHERE s{node_info[n_to]['id']}.uid = \"s{node_info[n_to]['id']}\" CREATE (s{node_info[n_from]['id']})-[:action " + "{" + f"aid: \"{action_info[action]['id']}\", probability:{self[n_from][n_to][action]['probability']}, frequency:{self[n_from][n_to][action]['frequency']}" + "}" + f"]->(s{node_info[n_to]['id']});"
 
         return graph_string
 
