9f4f20c1d245d1a69751070421b45825e960d975
Zach Parent <zachparent@duck.com>
2025-05-06 18:10:11 +0100

    add get_desires() for test_intention_aware_policy_graph
    Co-authored-by: Víctor Gimenez Abalos <victor.gimenez.abalos@gmail.com>
    Co-authored-by: Sara Montese <sara.montese@bsc.es>

--- test/pgeon/test_intention_aware_policy_graph.py
+++ test/pgeon/test_intention_aware_policy_graph.py
@@ -1,12 +1,64 @@
 import unittest
 from test.domain.test_env import State, TestingAgent, TestingDiscretizer, TestingEnv
+from typing import List
 
 import networkx as nx
 
 from pgeon import GraphRepresentation, Predicate
+from pgeon.desire import Desire
 from pgeon.discretizer import PredicateBasedStateRepresentation
 from pgeon.intention_aware_policy_graph import IPG
 from pgeon.policy_representation import Action
+
+# action_idx_to_name = {'0': 'UP', '1': 'DOWN', '2': 'RIGHT', '3': 'LEFT', '4': 'STAY', '5': 'Interact'}
+action_name_to_idx = {"Interact": "5"}
+
+
+def get_desires(only_one_pot=False) -> List[Desire]:
+    action_name_to_idx = {"Interact": "5"}
+    clause = {"HELD_PLAYER(SOUP)", "ACTION2NEAREST(SERVICE;INTERACT)"}
+    action = action_name_to_idx["Interact"]
+    desire_to_service = Desire("desire_to_service", action, clause)
+
+    clause = {
+        "HELD_PLAYER(ONION)",
+        "ACTION2NEAREST(POT0;INTERACT)",
+        "POT_STATE(POT0;PREPARING)",
+    }
+    action = action_name_to_idx["Interact"]
+    desire_to_cook0 = Desire("desire_to_cook0", action, clause)
+
+    clause = {
+        "HELD_PLAYER(ONION)",
+        "ACTION2NEAREST(POT0;INTERACT)",
+        "POT_STATE(POT0;NOT_STARTED)",
+    }
+    action = action_name_to_idx["Interact"]
+    desire_to_start_cooking0 = Desire("desire_to_start_cooking0", action, clause)
+
+    to_return = [desire_to_service, desire_to_cook0, desire_to_start_cooking0]
+
+    if not only_one_pot:
+        clause = {
+            "HELD_PLAYER(ONION)",
+            "ACTION2NEAREST(POT1;INTERACT)",
+            "POT_STATE(POT1;PREPARING)",
+        }
+        action = action_name_to_idx["Interact"]
+        desire_to_cook1 = Desire("desire_to_cook1", action, clause)
+
+        clause = {
+            "HELD_PLAYER(ONION)",
+            "ACTION2NEAREST(POT1;INTERACT)",
+            "POT_STATE(POT1;NOT_STARTED)",
+        }
+        action = action_name_to_idx["Interact"]
+        desire_to_start_cooking1 = Desire("desire_to_start_cooking1", action, clause)
+
+        to_return.append(desire_to_cook1)
+        to_return.append(desire_to_start_cooking1)
+
+    return to_return
 
 
 class TestIntentionAwarePolicyGraph(unittest.TestCase):
@@ -47,7 +99,20 @@ class TestIntentionAwarePolicyGraph(unittest.TestCase):
         self.assertEqual(len(self.graph.graph.edges), 0)
 
     def test_propogate_intentions(self):
-        pass
+        # load graph from edges and nodes files
+        desires = [
+            Desire(
+                "test_desire",
+                self.action0,
+                {Predicate(State, [State.ONE])},
+            )
+        ]
+        # this automatically calls propogate_intentions() on the nodes
+        self.graph.register_all_desires(desires)
+
+        # validate the intentions of several nodes have the expected values
+        self.assertEqual(self.graph.graph.nodes[self.state0].intention, 0)
+        self.assertEqual(self.graph.graph.nodes[self.state1].intention, 1)
 
 
 if __name__ == "__main__":
