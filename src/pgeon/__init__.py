5712647f711a3466c81edfd96a37c0c7e18efb63
Zach Parent <zachparent@duck.com>
2025-07-29 10:58:31 +0100

    update pgeon.__init__

--- src/pgeon/__init__.py
+++ src/pgeon/__init__.py
@@ -1,3 +1,12 @@
 from .agent import Agent
 from .discretizer import Discretizer, Predicate
-from .policy_graph import *
+from .intention_aware_policy_graph import IPG
+from .policy_graph import PolicyGraph
+
+__all__ = [
+    "Agent",
+    "Discretizer",
+    "Predicate",
+    "PolicyGraph",
+    "IPG",
+]
