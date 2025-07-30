7eb284ba5d6ebbadb4385ed9dfeff71c39b101b6
Zach Parent <zachparent@duck.com>
2025-07-30 14:08:00 +0100

    update __all__ in pgeon.__init__.py to export PGBasedPolicy classes

--- src/pgeon/__init__.py
+++ src/pgeon/__init__.py
@@ -1,7 +1,13 @@
 from .agent import Agent
 from .discretizer import Discretizer, Predicate
 from .intention_aware_policy_graph import IPG
-from .policy_graph import PolicyGraph
+from .policy_graph import (
+    PGBasedPolicy,
+    PGBasedPolicyMode,
+    PGBasedPolicyNodeNotFoundMode,
+    PolicyGraph,
+)
+from .policy_representation import GraphRepresentation, PolicyRepresentation
 
 __all__ = [
     "Agent",
@@ -9,4 +15,9 @@ __all__ = [
     "Predicate",
     "PolicyGraph",
     "IPG",
+    "PolicyRepresentation",
+    "GraphRepresentation",
+    "PGBasedPolicy",
+    "PGBasedPolicyMode",
+    "PGBasedPolicyNodeNotFoundMode",
 ]
